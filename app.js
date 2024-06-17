d588c850a57a8ce6d3b53723d27f7f01c0c167a1
wftb <145103446+wftb@users.noreply.github.com>
2024-06-17 09:59:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const https = require('https');
 const WebSocket = require('ws');
 const logcb = (...args) => console.log.bind(this, ...args);
 const errcb = (...args) => console.error.bind(this, ...args);
@@ -6,7 +7,27 @@ const errcb = (...args) => console.error.bind(this, ...args);
 const uuid = (process.env.UUID || '37a0bd7c-8b9f-4693-8916-bd1e2da0a817').replace(/-/g, '');
 const port = process.env.PORT || 3000;
 
-const wss = new WebSocket.Server({ port }, logcb('listen:', port));
+// Replace with your actual certificate and key
+const certificate = `-----BEGIN CERTIFICATE-----
+MIIDXTCCAkWgAwIBAgIJAMzZvdcNgk44MA0GCSqGSIb3DQEBCwUAMEUxCzAJBgNV
+...
+-----END CERTIFICATE-----`;
+
+const privateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAz3B8QDL2TgFc2VgiMd5yl5UVfLjbwnXKDkZ71Ef1n3Am9mPR
+...
+-----END RSA PRIVATE KEY-----`;
+
+const serverOptions = {
+    cert: certificate,
+    key: privateKey
+};
+
+// Create an HTTPS server
+const server = https.createServer(serverOptions);
+
+// Create a WebSocket server using the HTTPS server
+const wss = new WebSocket.Server({ server }, logcb('listen:', port));
 
 wss.on('connection', ws => {
     console.log("on connection");
@@ -20,9 +41,9 @@ wss.on('connection', ws => {
         let i = msg.slice(17, 18).readUInt8() + 19;
         const targetPort = msg.slice(i, i += 2).readUInt16BE(0);
         const ATYP = msg.slice(i, i += 1).readUInt8();
-        const host = ATYP === 1 ? msg.slice(i, i += 4).join('.') : // IPV4
-            (ATYP === 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) : // domain
-                (ATYP === 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : '')); // IPV6
+        const host = ATYP === 1 ? msg.slice(i, i += 4).join('.') : 
+            (ATYP === 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) :
+                (ATYP === 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : ''));
 
         logcb('conn:', host, targetPort);
 
@@ -35,4 +56,9 @@ wss.on('connection', ws => {
             duplex.on('error', errcb('E1:')).pipe(this).on('error', errcb('E2:')).pipe(duplex);
         }).on('error', errcb('Conn-Err:', { host, port: targetPort }));
     }).on('error', errcb('EE:'));
+});
+
+// Start the HTTPS server
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 });
